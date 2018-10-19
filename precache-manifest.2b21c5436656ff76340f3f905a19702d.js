0ec6759bc9b32d962300c435ebd51b554d86075e
Sebastian <wilk85@gmail.com>
2018-10-19 15:27:02 +0200

    Updates

---
+++ precache-manifest.2b21c5436656ff76340f3f905a19702d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd693ff12c32844ee75bcacb2deb23c",
+    "url": "/pogoda/static/media/bg.dcd693ff.jpg"
+  },
+  {
+    "revision": "efc503ef27764f817d41",
+    "url": "/pogoda/static/js/runtime~main.efc503ef.js"
+  },
+  {
+    "revision": "8e23eca3ac2f496c8956",
+    "url": "/pogoda/static/js/main.8e23eca3.chunk.js"
+  },
+  {
+    "revision": "8f71889b08b0ed118029",
+    "url": "/pogoda/static/js/1.8f71889b.chunk.js"
+  },
+  {
+    "revision": "8e23eca3ac2f496c8956",
+    "url": "/pogoda/static/css/main.cf06d90a.chunk.css"
+  },
+  {
+    "revision": "8f71889b08b0ed118029",
+    "url": "/pogoda/static/css/1.9f0020ba.chunk.css"
+  },
+  {
+    "revision": "99539ac79d2d00acd3df547d1291dd14",
+    "url": "/pogoda/index.html"
+  }
+];
